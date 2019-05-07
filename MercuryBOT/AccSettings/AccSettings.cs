26f9877aa65bee29b135ee71b40e5586fc304e69
sp0ok3r <digitalfoxanon@hotmail.com>
2019-05-07 17:37:11 +0100

    ++

--- MercuryBOT/AccSettings/AccSettings.cs
+++ MercuryBOT/AccSettings/AccSettings.cs
@@ -8,15 +8,6 @@
 ▐    ▐     ▐                  ▐                                 ▐   
 */
 using MercuryBOT.Helpers;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace MercuryBOT.AccSettings
 {
