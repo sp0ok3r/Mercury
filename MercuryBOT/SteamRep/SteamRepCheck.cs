431a8657b1d4fa1b9b9a89d9b74b230983a86385
sp0ok3r <digitalfoxanon@hotmail.com>
2019-05-07 12:58:44 +0100

    +

--- MercuryBOT/SteamRep/SteamRepCheck.cs
+++ MercuryBOT/SteamRep/SteamRepCheck.cs
@@ -9,12 +9,15 @@
 */
 
 using MercuryBOT.Helpers;
+using MercuryBOT.UserSettings;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
